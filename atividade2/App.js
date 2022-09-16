d42e6919825c700c6cd224de74e17cf3df3ad028
EduardoShoiti <shoitiaragaki@gmail.com>
2022-09-16 08:31:51 -0300

    Adicionado Views de descrição

--- atividade2/App.js
+++ atividade2/App.js
@@ -24,15 +24,49 @@ const Titulo = styled.Text`
 const Imagem = styled.Image`
 	width: 100%;
 	height: 200px;
-	align-self: center;
+	align-self: center;	
 `;
 
 const CaixaDescr = styled.View`
 	background-color: #ffffff;
-	width: 98%;
+	width: 100%;
 	height: 450px;
 	borderTopLeftRadius: 50px;
 	borderTopRightRadius: 50px;
+	
+	margin-top: 20%;		
+`;
+
+const NomeArma = styled.View`
+	color: #000000;
+	font-size: 26px;
+	font-weight: bold;
+	margin-vertical: 5%;
+	text-align: center;
+`;
+
+const TitDescrArma = styled.Text`
+	color: #000000;
+	font-size: 11px;
+	font-weight: bold;
+	padding-horizontal: 5%;
+	margin-bottom: 7%;
+`;
+
+const DescArma = styled.Text`
+	color: #4F4F4F;
+	font-size: 11px;
+	padding-horizontal: 7%;
+	margin-top: 5%;
+	margin-left: 5%;
+`;
+const MarcadorDesc = styled.Text`
+	color: #000000;
+	font-size: 26px;
+	font-weight: bold;
+	margin-left: 2%;
+	position: relative;
+	top: 10%;
 `;
 
 export default function App() {
@@ -44,7 +78,12 @@ export default function App() {
 				<Imagem source={arma} />
 
 				<CaixaDescr>
-					<Titulo>AA</Titulo>
+					<NomeArma>SHERIFF</NomeArma>
+					<TitDescrArma>Sheriff é um revólver moderno feito para quem busca tiros certeiros na cabeça do inimigo.</TitDescrArma>
+					<MarcadorDesc> - </MarcadorDesc>
+					<DescArma> Seus projéteis de alto impacto provocam bastante recuo, então é preciso ter pulso firme. </DescArma>
+					<MarcadorDesc> - </MarcadorDesc>
+					<DescArma> Quando você domina esta arma, seus inimigos logo percebem que nunca tiveram chance. </DescArma>
 				</CaixaDescr>
 			</Container>
 		</SafeContainer>
